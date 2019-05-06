48ee8fe9e1d93c8abd28340ebe56b9dbfae513c9
xurq2 <xurq2@lenovo.com>
2019-05-06 17:06:25 +0800

    chushihua

--- customizelifecycle/src/main/java/com/bolingcavalry/customizelifecycle/CustomizelifecycleApplication.java
+++ customizelifecycle/src/main/java/com/bolingcavalry/customizelifecycle/CustomizelifecycleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomizelifecycleApplication {
-
+	// org.springframework.context.support.AbstractApplicationContext.finishRefresh()
 	public static void main(String[] args) {
 		SpringApplication.run(CustomizelifecycleApplication.class, args);
 	}
--- customizepropertyverify/src/main/java/com/bolingcavalry/customizepropertyverify/CustomizepropertyverifyApplication.java
+++ customizepropertyverify/src/main/java/com/bolingcavalry/customizepropertyverify/CustomizepropertyverifyApplication.java
@@ -1,15 +1,26 @@
 package com.bolingcavalry.customizepropertyverify;
 
-import com.bolingcavalry.customizepropertyverify.context.CustomApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bolingcavalry.customizepropertyverify.context.CustomApplicationContext;
+
 @SpringBootApplication
 public class CustomizepropertyverifyApplication {
+	// org.springframework.context.support.AbstractApplicationContext
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(CustomizepropertyverifyApplication.class);
-        springApplication.setApplicationContextClass(CustomApplicationContext.class);
-        springApplication.run(args);
-    }
+		// Properties properties = System.getProperties();
+		// Iterator it = properties.entrySet().iterator();
+		// while (it.hasNext()) {
+		// Entry entry = (Entry) it.next();
+		// System.out.print(entry.getKey() + "=");
+		// System.out.println(entry.getValue());
+		// }
+
+		System.setProperty("MYSQL_HOST", "1111");
+		SpringApplication springApplication = new SpringApplication(CustomizepropertyverifyApplication.class);
+		springApplication.setApplicationContextClass(CustomApplicationContext.class);
+		springApplication.run(args);
+	}
 }
